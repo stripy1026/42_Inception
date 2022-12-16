7888a3b60745c96072597bd4bca19dea1902911a
stripy1026 <inwoo1026@naver.com>
2022-12-16 16:49:33 +0900

    test: :construction: Mariadb set, not tested
    IDK much about query

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,1 +1,18 @@
-#script
+# !/bin/sh
+
+cat .setup 2> /dev/null
+
+if [ $? -ne 0 ]; then
+    /usr/bin/mysql_safe --datadir=/var/lib/mysql &
+    sed -i "s/skip-networking/# skip-networking/g" /etc/my.cnf.d/mariadb-server.cnf
+    sed -i "s/.*bind-address\s*=.*/bind-address=0.0.0.0\nport=3306/g" /etc/my.cnf.d/mariadb-server.cnf
+    if ! mysqladmin --wait=30 ping; then
+        printf "MariaDB Daemon Unreachable\n"
+        exit 1
+    fi
+    eval "echo \"#(cat /tmp/query.sql)\"" | mariadb
+    pkill mariadb
+    touch .setup
+fi
+
+/usr/bin/mysql_safe --datadir=/var/lib/mysql
